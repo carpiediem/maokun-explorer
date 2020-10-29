a8074809d0515f536a4dd0589c2350639ac54208
HPEnvy\Ryan <ryan@rslc.us>
2020-10-29 17:01:26 +0800

    dependency cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { IntlProvider } from 'react-intl';
-// import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import { createBrowserHistory } from 'history';
 
 import Explorer from './screens/Explorer';
 
@@ -15,22 +13,10 @@ const messages = {
 
 function App() {
   const [locale] = React.useContext(LocaleContext);
-  // const history = createBrowserHistory();
-
-  // history.listen((location) => {
-  //   window.gtag('set', 'page', location.pathname + location.search);
-  //   window.gtag('send', 'pageview');
-  // });
 
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
-      {/* <BrowserRouter history={history}>
-        <Switch>
-          <Route path="/"> */}
       <Explorer />
-      {/* </Route>
-        </Switch>
-      </BrowserRouter> */}
     </IntlProvider>
   );
 }
